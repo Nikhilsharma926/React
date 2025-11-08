14b7af6a36dd43f9b32f874d3dce43ff97b08249
Nikhilsharam962 <niksharma787683@gmail.com>
2025-11-08 23:02:36 +0530

    component based struture

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+
+
+const Card = () => {
+  return (
+    <div>
+    <div className="Card">
+        <h1>Nikhil sharma</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      
+    </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='Nav'>
+            <h1>this is Navbar</h1>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
