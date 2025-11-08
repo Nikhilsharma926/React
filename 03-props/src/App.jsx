08c5cdbd7cc5e62fb1d9222e87e6ee8aa784157a
Nikhilsharam962 <niksharma787683@gmail.com>
2025-11-09 00:27:43 +0530

    finally i done props

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './components/Card'
+const App = () => {
+  
+  return (
+    <div>
+      <Card user = 'nikhil' age ={12} img='https://images.unsplash.com/photo-1761582286153-03b935a8a41e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ODc1MjAzMjB8fGVufDB8fHx8fA%3D%3D%2C%5Bobject+Object%5D&auto=format&fit=crop&q=60&w=294&dpr=2&h=294' / > 
+      <Card user = 'shiv' age ={11} img=' https://images.unsplash.com/photo-1649859115675-c9180baa9cfc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8WThJdXVaNksta298fGVufDB8fHx8fA%3D%3D%2C%5Bobject+Object%5D&auto=format&fit=crop&q=60&w=294&dpr=2&h=294'   /  > 
+      <Card user = 'anish' age ={12}img ='https://images.unsplash.com/photo-1637272507809-17a866921aa1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnxZOEl1dVo2Sy1rb3x8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600 '   / > 
+      <Card user = 'abhishek' age ={11}img ='https://images.unsplash.com/photo-1641071800065-470803dba92c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3xZOEl1dVo2Sy1rb3x8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600 '  /  > 
+
+      
+      
+    </div>
+  )
+}
+
+export default App
+
